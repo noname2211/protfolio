8491fcb06a18be6e3685006caa78ce05816c308d
NoorZak <n.zakarneh1@student.aaup.edu>
2020-12-22 16:17:51 +0200

    commit10

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.root),
+    path('i', views.index),
+    path('r', views.resumeForUser),
+    path('skills/view',views.skillsView) ,
+    path('skills/edit',views.skillsEdit) ,
+    path('skills/insert',views.skillsEdit),
+    path('experience/view', views.experienceView),
+    path('experience/edit', views.experienceEdit),
+    path('experience/insert', views.experienceInsert),
+    path('contacts/view', views.contactsView),
+    path('contacts/edit', views.contactsEdit),
+    path('contacts/insert', views.contactsInsert),
+    path('users/view', views.usersView),
+    path('users/edit', views.usersEdit),
+    path('users/insert', views.usersInsert),
 
 ]
--- main/views.py
+++ main/views.py
@@ -5,10 +5,103 @@ from django.urls import path,include
 from django.shortcuts import render
 
 # Create your views here.
+
+def resumeForUser(request):
+    return  render(request,"resumeForUser.html")
+
+def index(request):
+    return  render(request,"index.html")
 def root(request):
     context={
         "view": "header"
 
 
+    }
+    return render(request,"temp.html",context)
+
+
+def skillsView(request):
+    context={
+        "view":"skills/view"
+    }
+    return render(request,"temp.html",context)
+
+def skillsEdit(request):
+    context={
+        "view":"skills/edit"
+    }
+    return render(request,"temp.html",context)
+
+
+def skillsInsert(request):
+    context={
+        "view":"skills/insert"
+
+    }
+
+    return render(request,"temp.html",context)
+
+
+
+def experienceView(request):
+    context={
+        "view":"experience/view"
+    }
+    return render(request,"temp.html",context)
+
+
+def experienceEdit(request):
+    context={
+        "view":"experience/edit"
+    }
+    return render(request,"temp.html",context)
+
+
+def experienceInsert(request):
+    context={
+        "view":"experience/insert"
+            }
+    return render(request,"temp.html",context)
+
+
+
+def contactsView(request):
+    context={
+        "view":"contacts/view"
+            }
+    return render(request,"temp.html",context)
+
+
+def contactsEdit(request):
+    context={
+        "view":"contacts/edit"
+            }
+    return render(request,"temp.html",context)
+
+
+def contactsInsert(request):
+    context={
+        "view":"contacts/insert"
+    }
+    return render(request,"temp.html",context)
+
+
+def usersView(request):
+    context={
+        "view":"users/view"
+            }
+    return render(request,"temp.html",context)
+
+
+def usersEdit(request):
+    context={
+        "view":"users/edit"
+            }
+    return render(request,"temp.html",context)
+
+
+def usersInsert(request):
+    context={
+        "view":"users/insert"
     }
     return render(request,"temp.html",context)
