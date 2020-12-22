196ab78a0d400c8f61c91fe41a5f7db859e8dbb5
NoorZak <n.zakarneh1@student.aaup.edu>
2020-12-22 13:48:23 +0200

    commit3

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.urls import path,include
 
 # Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
 def root(request):
-    return render(request,"index.html")
+    context={
+        "view": "header"
+
+
+    }
+    return render(request,"temp.html",context)
